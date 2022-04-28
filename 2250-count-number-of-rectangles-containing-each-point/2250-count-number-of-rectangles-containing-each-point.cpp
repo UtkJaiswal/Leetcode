c35d5d7f0ab013ffd85ddd6217d7ae05f6e7793a
Utkarsh Jaiswal <51295665+UtkJaiswal@users.noreply.github.com>
2022-04-28 16:24:27 +0530

    Time: 1709 ms (5.06%), Space: 91 MB (34.55%) - LeetHub

--- 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    vector<int> countRectangles(vector<vector<int>>& rect, vector<vector<int>>& points) {
-        unordered_map<int,vector<int>>m;
-        for(int i=0;i<rect.size();i++)
-        {
-            m[rect[i][1]].push_back(rect[i][0]);
+    vector<int> countRectangles(vector<vector<int>>& rect, vector<vector<int>>& points) 
+    {
+        unordered_map<int, vector<int>> htl;
+        // maps heights to all the lengths of rectangles with that height
+        
+        for(int i=0; i<rect.size(); i++){       
+            htl[rect[i][1]].push_back(rect[i][0]);
         }
         
-        for(int i=1;i<=100;i++)
-            sort(m[i].begin(),m[i].end());
+		// have to sort the containers to apply binary search
+        for(int i=0; i<=100; i++){
+            sort(htl[i].begin(), htl[i].end());
+        }
         
-        vector<int>ans;
-        for(auto itr:points)
-        {
-            int x=itr[0];
-            int y=itr[1];
-            int c=0;
-            for(int j=y;j<=100;j++)
-            {
-                if(m.count(j))
-                {
-                    int p=lower_bound(m[j].begin(),m[j].end(),x)-m[j].begin();
-                    // cout<<j<<" "<<p<<endl;
-                    c+=m[j].size()-p;
+		
+        vector<int> ans;
+		
+        for(vector<int> p: points){
+            int x=p[0], y=p[1];
+            int ct=0;
+            for(int j= y; j<=100; j++){
+                if(htl.find(j)!=htl.end()){
+                    int xxx=lower_bound(htl[j].begin(),htl[j].end(),x)-htl[j].begin();
+                    //if(xxx==-1) xxx=0;
+                    ct+= htl[j].size()- xxx;
+                    //ct+= lower_bound(htl[j].begin(),htl[j].end(),x)-htl[j].begin();
+					// binary search return the idx in array from which the values are >= x
+					// the values at this and right of this are the lengths possible
+					// so substract by size of array to get the number
                 }
             }
-            ans.push_back(c);
+            
+            ans.push_back(ct);
+            
         }
-        return ans;
+        
+        return ans;    
     }
 };
