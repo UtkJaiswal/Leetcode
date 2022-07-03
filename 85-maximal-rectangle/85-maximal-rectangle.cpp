3a452f6f7bda3b7194f6491611a1cc1d0a71c713
Utkarsh Jaiswal <51295665+UtkJaiswal@users.noreply.github.com>
2022-07-03 14:20:20 +0530

    Time: 68 ms (57.57%), Space: 15.7 MB (36.21%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,41 +1,63 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(vector<vector<int>>&a,int l,int r)
-    {
-        if(l>r)
-            return 0;
-        
-        if(dp[l][r]!=-1)
-            return dp[l][r];
-        
-        int ans=0,s=r-l+1,x=0,n=a.size();
-        for(int i=1;i<n;i++)
-        {
-            if(a[i][r]-a[i][l-1]<s)
-            {
-                ans=max(ans,x*s);
-                x=0;
-                continue;
-            }
-            x++;
-        }
-        ans=max(ans,s*x);
-        return dp[l][r]=max({ans,solve(a,l+1,r),solve(a,l,r-1)});
-    }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
-        vector<vector<int>>a(n+1,vector<int>(m+1,0));
-        
-        for(int i=1;i<=n;i++)
+        vector<vector<int>>a(n,vector<int>(m,0));
+        for(int i=0;i<m;i++)
         {
-            for(int j=1;j<=m;j++)
+            a[0][i]=matrix[0][i]=='1';
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
             {
-                a[i][j]+=a[i][j-1]+(matrix[i-1][j-1]=='1');
+                if(matrix[i][j]=='1')
+                    a[i][j]=a[i-1][j]+1;
+                else
+                    a[i][j]=0;
             }
         }
-        dp=vector<vector<int>>(m+1,vector<int>(m+1,-1));
-        return solve(a,1,m);
+        
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            stack<int>st;
+            vector<int>l(m,0);
+            vector<int>r(m,0);
+            for(int j=0;j<m;j++)
+            {
+                while(!st.empty() && a[i][st.top()]>=a[i][j])
+                    st.pop();
+                
+                if(st.empty())
+                    l[j]=-1;
+                else
+                    l[j]=st.top();
+                
+                st.push(j);
+            }
+            while(!st.empty())
+                st.pop();
+            for(int j=m-1;j>=0;j--)
+            {
+                while(!st.empty() && a[i][st.top()]>=a[i][j])
+                    st.pop();
+                
+                if(st.empty())
+                    r[j]=m;
+                
+                else
+                    r[j]=st.top();
+                
+                st.push(j);
+            }
+            
+            for(int j=0;j<m;j++)
+            {
+                ans=max(ans,(r[j]-l[j]-1)*a[i][j]);
+            }
+        }
+        return ans;
         
     }
 };
