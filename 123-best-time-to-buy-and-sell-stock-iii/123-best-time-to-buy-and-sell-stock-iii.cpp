680729185c664edd40dd734866587735ede1ebe6
Utkarsh Jaiswal <51295665+UtkJaiswal@users.noreply.github.com>
2022-07-06 01:59:39 +0530

    Time: 1094 ms (19.22%), Space: 206.9 MB (31.77%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,39 +2,39 @@ class Solution {
 public:
     int maxProfit(vector<int>& A) {
         int n=A.size();
-    vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(2,-1)));
-    // for(int i=0;i<=n;i++)
-    // {
-    //     for(int j=0;j<2;j++)
-    //     dp[i][j][0]=0;
-    // }
-    for(int c=0;c<2;c++)
-    {
-        for(int left=1;left<=2;left++)
-        {
-            dp[n][c][left-1]=0;
-        }
-    }
-    for(int i=n-1;i>=0;i--)
-    {
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(2,-1)));
+        // for(int i=0;i<=n;i++)
+        // {
+        //     for(int j=0;j<2;j++)
+        //     dp[i][j][0]=0;
+        // }
         for(int c=0;c<2;c++)
         {
-            for(int left=2;left>=1;left--)
+            for(int left=1;left<=2;left++)
             {
-                if(c==0)
-                dp[i][c][left-1]=max(-A[i]+dp[i+1][1][left-1],dp[i+1][0][left-1]);
-                else
-                {
-                    if(left-2>=0)
-                    dp[i][c][left-1]=max(A[i]+dp[i+1][0][left-2],dp[i+1][1][left-1]);
-                    else
-                    dp[i][c][left-1]=max(A[i]+0,dp[i+1][1][left-1]);
-                }
-                
+                dp[n][c][left-1]=0;
             }
         }
-    }
-    return dp[0][0][1];
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int c=0;c<2;c++)
+            {
+                for(int left=2;left>=1;left--)
+                {
+                    if(c==0)
+                    dp[i][c][left-1]=max(-A[i]+dp[i+1][1][left-1],dp[i+1][0][left-1]);
+                    else
+                    {
+                        if(left-2>=0)
+                        dp[i][c][left-1]=max(A[i]+dp[i+1][0][left-2],dp[i+1][1][left-1]);
+                        else
+                        dp[i][c][left-1]=max(A[i]+0,dp[i+1][1][left-1]);
+                    }
+
+                }
+            }
+        }
+        return dp[0][0][1];
         
     }
 };
